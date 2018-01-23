47be37a5c571a6beb3bcf5e1ad1e7ec6b5dd0c8d
Niko Partanen <nikotapiopartanen@gmail.com>
2018-01-23 09:39:26 +0100

    changes here

--- R/data.R
+++ R/data.R
@@ -1,18 +1,82 @@
 #' Public Domain Komi-Zyrian data
 #'
-#' A dataset containing 78897 sentences from Public Domain sources.
+#' A dataset containing sentences from Public Domain sources.
 #' Digitalized in Fenno-Ugrica and proofread in FU-Lab.
-#' Ivan Belykh's works made available by the author.
+#' Ivan Belykh's works made available by the author. Four Battles
+#' book proofread by Niko Partanen. Current version contains only data
+#' comparable with other languages, also the idea is to have all possible
+#' Komi-Zyrian data here and the sentence_id would tell which are
+#' matching.
 #'
-#' @format Data frame with 78897 sentences.
+#' @format Data frame with Komi-Zyrian data
 #' \describe{
+#'   \item{doc_id}{name of the text in original corpus}
 #'   \item{sentence_id}{sentence id, unique within a text}
 #'   \item{sentence}{sentence text}
-#'   \item{token}{token in lowercase}
-#'   \item{text_name}{name of the text in original corpus}
 #'   ...
 #' }
 #' @source \url{https://github.com/langdoc/kpv-lit}
 #' @source \url{https://fennougrica.kansalliskirjasto.fi/}
 #' @source \url{http://komikyv.org/}
 "kpv"
+
+#' Public Domain Komi-Permyak data
+#'
+#' A dataset containing sentences from Public Domain sources.
+#' Four Battles book proofread by Niko Partanen.
+#'
+#' @format Data frame with a Komi-Permyak book
+#' \describe{
+#'   \item{doc_id}{name of the text in original corpus}
+#'   \item{sentence_id}{sentence id, unique within a text}
+#'   \item{sentence}{sentence text}
+#'   ...
+#' }
+#' @source \url{http://urn.fi/URN:NBN:fi-fe2014101045137}
+"koi"
+
+#' Public Domain Udmurt data
+#'
+#' A dataset containing sentences from Public Domain sources.
+#' Four Battles book proofread by Niko Partanen.
+#'
+#' @format Data frame with an Udmurt book
+#' \describe{
+#'   \item{doc_id}{name of the text in original corpus}
+#'   \item{sentence_id}{sentence id, unique within a text}
+#'   \item{sentence}{sentence text}
+#'   ...
+#' }
+#' @source \url{http://urn.fi/URN:NBN:fi-fe2014092444879}
+"udm"
+
+#' Public Domain Erzya data
+#'
+#' A dataset containing sentences from Public Domain sources.
+#' Four Battles book proofread by Niko Partanen.
+#'
+#' @format Data frame with an Erzya book
+#' \describe{
+#'   \item{doc_id}{name of the text in original corpus}
+#'   \item{sentence_id}{sentence id, unique within a text}
+#'   \item{sentence}{sentence text}
+#'   ...
+#' }
+#' @source \url{http://urn.fi/URN:NBN:fi-fe2014082633380}
+"myv"
+
+#' Public Domain Meadow Mari data
+#'
+#' A dataset containing sentences from Public Domain sources.
+#' Four Battles book proofread by Niko Partanen.
+#'
+#' @format Data frame with a Meadow Mari book
+#' \describe{
+#'   \item{doc_id}{name of the text in original corpus}
+#'   \item{sentence_id}{sentence id, unique within a text}
+#'   \item{sentence}{sentence text}
+#'   ...
+#' }
+#' @source \url{http://urn.fi/URN:NBN:fi-fe2014091844781}
+"mhr"
+
